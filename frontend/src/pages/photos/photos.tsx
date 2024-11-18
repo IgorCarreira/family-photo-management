d3fa36112d4e44904bb9edc12b5a9763dbd2545f
igor.picolo <igor.picolo@eurekalabs.com.br>
2024-11-18 10:22:37 -0300

    fix: layout

--- frontend/src/pages/photos/photos.tsx
+++ frontend/src/pages/photos/photos.tsx
@@ -103,7 +103,7 @@ export const Photos = () => {
             </Button>
           )}
         </div>
-        <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 ">
+        <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
           {data?.photos?.map((photo) => (
             <PhotoCard
               photo={photo}
